2f2e3fa5f6ebbf4760923ca01e25b6f00316d923
Davidkane <23145087+davidkane0526@users.noreply.github.com>
2026-09-20 22:15:52 +0800

    chore: sync runtime version 3.71.107

--- src/plugin-window/runtime.js
+++ src/plugin-window/runtime.js
@@ -601,7 +601,7 @@
 
   function baseHost() {
     return {
-      appVersion:'3.71.106',
+      appVersion:'3.71.107',
       isAuxiliaryWindow:true,
       isWebClient:false,
       renderActivityNavigation:()=>window.DKDSDesktopPresentationShell?.renderNavigation?.({isAuxiliaryWindow:true}),
