7df6750183b68dbf71ca30ae211e3388af97f6d0
Davidkane <23145087+davidkane0526@users.noreply.github.com>
2026-09-23 12:35:21 +0800

    fix: synchronize plugin window version

--- src/plugin-window/runtime.js
+++ src/plugin-window/runtime.js
@@ -601,7 +601,7 @@
 
   function baseHost() {
     return {
-      appVersion:'3.71.114',
+      appVersion:'3.71.115',
       isAuxiliaryWindow:true,
       isWebClient:false,
       renderActivityNavigation:()=>window.DKDSDesktopPresentationShell?.renderNavigation?.({isAuxiliaryWindow:true}),
