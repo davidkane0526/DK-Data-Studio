06a8be7de74a9fd18a8281e2683991df74f903c2
Davidkane <23145087+davidkane0526@users.noreply.github.com>
2026-09-26 01:36:36 +0800

    fix: synchronize 3.71.116 runtime version surfaces

--- src/plugin-window/runtime.js
+++ src/plugin-window/runtime.js
@@ -584,7 +584,7 @@
 
   function baseHost() {
     return {
-      appVersion:'3.71.115',
+      appVersion:'3.71.116',
       isAuxiliaryWindow:true,
       isWebClient:false,
       renderActivityNavigation:()=>window.DKDSDesktopPresentationShell?.renderNavigation?.({isAuxiliaryWindow:true}),
